45df48e0855c1b6d15d24f79dd22772e6439026a
Casey Siebel <leptone55@gmail.com>
2017-11-14 15:18:25 -0800

    Update Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './style.scss';
 
+import logo from '../../assets/dr-logo-white.png';
+
 class Header extends React.Component {
     render() {
         return (
             <div className='header'>
+                <img src={ logo }/>
             </div>
         );
     } 
