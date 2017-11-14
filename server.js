50f5f65e18647d3d96c258b68a378799859e5df1
Casey Siebel <leptone55@gmail.com>
2017-11-14 15:52:39 -0800

    Fix static assets route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + './dist'));
+app.use(express.static(__dirname + '/dist'));
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => console.log('Running on port', PORT));
