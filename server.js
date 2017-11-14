b10280794791c7401dc10269c1d422daf8331cb9
Casey Siebel <leptone55@gmail.com>
2017-11-14 15:39:07 -0800

    correct statics dir

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + './dir'));
+app.use(express.static(__dirname + './dist'));
 
 const PORT = process.env.PORT || 80;
 const server = app.listen(PORT, () => console.log('Running on port', PORT));
