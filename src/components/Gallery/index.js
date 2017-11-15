314d5e7d2d64a19f4b83328b6e7efe139e9d5e89
Casey Siebel <leptone55@gmail.com>
2017-11-14 22:54:53 -0800

    Start Gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-//import { Link } from 'react-router';
-
 import './style.scss';
+
+import img from '../../assets/waiting.jpg';
 
 class Gallery extends React.Component {
     render() {
         return (
-            <div className='gallery'>
-            </div>
+            <svg className='gallery'>
+                <defs>
+                    <rect className='trapizoid'/>
+                    <rect className='middle'/>
+                    <rect className='bottom'/>
+                    <rect className='right'/>
+                </defs>
+            </svg>
         );
     } 
 }
