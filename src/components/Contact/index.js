125a51a3a5f3abe23188e1000ddf516fe8ce109e
Casey Siebel <leptone55@gmail.com>
2017-11-15 21:38:09 -0800

    Style Contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,11 +7,16 @@ class Contact extends React.Component {
             <div className='contact'>
                 <h1 className='component-title'>We Would Love to Hear From You</h1>
                 <form>
-                    <input name='name' type='text' className='name' />
-                    <input name='email' type='text' className='email' />
-                    <textarea name='message' className='message' />
-                    <input name='captcha' type='checkbox' className='captcha' />
-                    <button>Submit</button>
+                    <input placeholder='Name' name='name' type='text' className='name' />
+                    <input placeholder='E-mail' name='email' type='text' className='email' />
+                    <textarea placeholder='Message...' name='message' className='message' />
+                    <div className='submit-row'>
+                        <div>
+                            <input name='captcha' type='checkbox' className='captcha' id='captcha' />
+                            <label for='captcha'>I am not a robot</label>
+                        </div>
+                        <button>Submit</button>
+                    </div>
                 </form>
             </div>
         );
