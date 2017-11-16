fb33dfabd8f44c3ca01e50b51d355dd1029b825a
Casey Siebel <leptone55@gmail.com>
2017-11-16 15:54:14 -0800

    Add buttons class with CSS grid

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -14,8 +14,10 @@ class Event extends React.Component {
                     <p>Date of party</p>
                     <p>Location of party</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <button>Buy Tickets</button>
-                    <button>Facebook</button>
+                    <div className='buttons'>
+                        <button>Buy Tickets</button>
+                        <button>Facebook</button>
+                    </div>
                 </div>
             </div>
         );
