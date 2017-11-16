a3e7c31cda4f4703e0af9afe585fc8a52e1bdc67
Casey Siebel <leptone55@gmail.com>
2017-11-15 21:03:37 -0800

    Add component-title className

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,7 @@ class Contact extends React.Component {
     render() {
         return (
             <div className='contact'>
-                <h1>We Would Love to Hear From You</h1>
+                <h1 className='component-title'>We Would Love to Hear From You</h1>
                 <form>
                     <input name='name' type='text' className='name' />
                     <input name='email' type='text' className='email' />
